7fe850cefaa878595f70bfdf763134e490bda829
Kennedy Mbano <kenmbano@hotmail.com>
2024-04-12 19:12:46 -0400

    project setup

---
+++ book_store/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace book_store.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
